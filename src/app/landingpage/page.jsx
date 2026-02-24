6b8c5c8f5fe0e40e3142d93f35792bc84b68cf2a
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2026-02-24 14:14:09 -0300

    Add landing page, proxy and Turbopack root

---
+++ src/app/landingpage/page.jsx
@@ -0,0 +1,11 @@
+import styles from "./landingpage.module.css";
+
+export default function LandingPage() {
+    return (
+        <div className={styles.container}>
+            <h1>Oi</h1>
+            <p>sei lá</p>
+        </div>
+    );
+}
+    
