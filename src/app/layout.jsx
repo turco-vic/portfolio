8789da7cd2d046795916ee377f5975afc3b32959
Enzo Turcovic <enzoturcovic@gmail.com>
2026-03-02 19:24:10 -0300

    Add Vercel Speed Insights

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import Nav from '@/components/Nav'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: 'Portfólio — Enzo Turcovic',
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
         <Nav />
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
